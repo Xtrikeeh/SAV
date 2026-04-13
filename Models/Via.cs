2c59550a4d01436939467aab7a2facde62ae8907
Xtrikeeh <bmols2006@gmail.com>
2026-04-13 12:43:55 -0300

    Layout básico feito - menu superior, menu lateral esquerdo(ferramentas) e protótipo de sistema de leitura de arquivo de mapas.

---
+++ Models/Via.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Media;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SAV.Models
+{
+    public class Via : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public PointCollection Pontos { get; set; } // Desenho da linha
+
+        private int _velocidade;
+        public int Velocidade
+        {
+            get => _velocidade;
+            set { _velocidade = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SAV.Models;
+using SAV.ViewModels;
+using System.IO;
+using System.Windows.Media;
 
 namespace SAV
 {
@@ -16,6 +13,44 @@ namespace SAV
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel(); // Liga o XAML à ViewModel
+        }
+
+        private void Via_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var polyline = sender as Polyline;
+            var via = polyline.DataContext as Via;
+            var vm = this.DataContext as MainViewModel;
+            vm.ViaSelecionada = via; // Coloca a via no painel lateral
+        }
+
+        // Mude de BotaoAbrir_Click para AbrirMapa_MouseDown
+        private void AbrirMapa_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Criamos o explorador de arquivos do Windows
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Arquivos JSON (*.json)|*.json";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // 1. Pegamos a nossa ViewModel
+                var viewModel = this.DataContext as MainViewModel;
+
+                // 2. Chamamos a função de ler o arquivo (aquela que usa o Newtonsoft.Json)
+                // Se você ainda não criou a função AbrirMapa na ViewModel, 
+                // use o código abaixo para teste rápido:
+                var pontos = new System.Windows.Media.PointCollection {
+            new Point(100, 100),
+            new Point(400, 200)
+        };
+
+                viewModel.MinhasVias.Add(new Via
+                {
+                    Nome = "Rua Importada",
+                    Velocidade = 60,
+                    Pontos = pontos
+                });
+            }
         }
     }
 }
