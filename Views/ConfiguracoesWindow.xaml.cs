1861a8c05b95949d532054b1003edb28f8d708ba
Xtrikeeh <bmols2006@gmail.com>
2026-04-15 12:08:32 -0300

    Adicionado janela de configurações do sistema.

---
+++ Views/ConfiguracoesWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Win32;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SAV.Views
+{
+    public partial class ConfiguracoesWindow : Window
+    {
+        public ConfiguracoesWindow()
+        {
+            InitializeComponent();
+
+            inicializarConfiguracoes();
+        }
+
+        // Botão trocar tema
+        private void trocarTema_Checked(object sender, EventArgs e)
+        {
+            if (trocarTema.Content == "Claro")
+            {
+                var conversorBrush = new BrushConverter();
+
+                Application.Current.Resources["corDestaquePrimaria"] = (Brush)conversorBrush.ConvertFrom("#1B1B1B")!;
+                Application.Current.Resources["corDestaqueSecundaria"] = (Brush)conversorBrush.ConvertFrom("#F5F5F5")!;
+                Application.Current.Resources["corDestaqueTerciaria"] = (Brush)conversorBrush.ConvertFrom("#292929")!;
+                Application.Current.Resources["corFonte"] = (Brush)conversorBrush.ConvertFrom("#F5F5F5")!;
+                trocarTema.Content = "Escuro";
+
+                Properties.Settings.Default.TemaEscuro = true;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void trocarTema_Unchecked(object sender, EventArgs e)
+        {
+            if (trocarTema.Content == "Escuro")
+            {
+                var conversorBrush = new BrushConverter();
+
+                Application.Current.Resources["corDestaquePrimaria"] = (Brush)conversorBrush.ConvertFrom("#F5F5F5")!;
+                Application.Current.Resources["corDestaqueSecundaria"] = (Brush)conversorBrush.ConvertFrom("#1B1B1B")!;
+                Application.Current.Resources["corDestaqueTerciaria"] = (Brush)conversorBrush.ConvertFrom("#D4D4D4")!;
+                Application.Current.Resources["corFonte"] = (Brush)conversorBrush.ConvertFrom("#1C2639")!;
+                trocarTema.Content = "Claro";
+
+                Properties.Settings.Default.TemaEscuro = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        // Botão opções salvamento automático
+        private void botaoSalvamentoAutomatico_Click(object sender, EventArgs e)
+        {
+            Button botaoSalvamento = sender as Button;
+            botaoSalvamento.ContextMenu.IsEnabled = true;
+            botaoSalvamento.ContextMenu.PlacementTarget = botaoSalvamento;
+            botaoSalvamento.ContextMenu.IsOpen = true;
+        }
+
+        private void opcaoSalvamentoAutomatico_Click(object sender, EventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+
+            string opcao = item.Tag.ToString()!;
+
+            if (opcao == "5")
+            {
+                Properties.Settings.Default.TempoSalvamento = 5;
+                Properties.Settings.Default.Save();
+            } 
+            else if (opcao == "10")
+            {
+                Properties.Settings.Default.TempoSalvamento = 10;
+                Properties.Settings.Default.Save();
+            }
+            else if (opcao == "15")
+            {
+                Properties.Settings.Default.TempoSalvamento = 15;
+                Properties.Settings.Default.Save();
+            }
+            else if (opcao == "30")
+            {
+                Properties.Settings.Default.TempoSalvamento = 30;
+                Properties.Settings.Default.Save();
+            }
+            else if (opcao == "Nunca")
+            {
+                Properties.Settings.Default.TempoSalvamento = 0;
+                Properties.Settings.Default.Save();
+            }
+
+            int tempoSalvamento = Properties.Settings.Default.TempoSalvamento;
+
+            if (tempoSalvamento == 0)
+            {
+                botaoSalvamento.Content = "Nunca";
+            }
+            else
+            {
+                botaoSalvamento.Content = $"{tempoSalvamento.ToString()} minutos";
+            }
+        }
+
+        private void inicializarConfiguracoes()
+        {
+            bool temaEscuro = Properties.Settings.Default.TemaEscuro;
+            int tempoSalvamento = Properties.Settings.Default.TempoSalvamento;
+            string diretorioPadrao = Properties.Settings.Default.DiretorioPadrao;
+
+            if (tempoSalvamento == 0)
+            {
+                botaoSalvamento.Content = "Nunca";
+            }
+            else
+            {
+                botaoSalvamento.Content = $"{tempoSalvamento.ToString()} minutos";
+            }
+
+            trocarTema.IsChecked = temaEscuro;
+
+            trocarTema.Content = temaEscuro ? "Escuro" : "Claro";
+
+            textoDiretorio.Text = diretorioPadrao;
+        }
+
+        private void botaoCaminho_Click(object sender, EventArgs e)
+        {
+            OpenFolderDialog selecionarPasta = new OpenFolderDialog();
+            selecionarPasta.Title = "Selecione o diretório padrão";
+
+            if (selecionarPasta.ShowDialog() == true)
+            {
+                string pastaSelecionada = selecionarPasta.FolderName;
+
+                Properties.Settings.Default.DiretorioPadrao = pastaSelecionada;
+                Properties.Settings.Default.Save();
+
+                textoDiretorio.Text = pastaSelecionada;
+            }
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Windows;
+﻿using SAV.Models;
+using SAV.Views;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
-using System.Windows.Shapes;
-using SAV.Models;
-using SAV.ViewModels;
-using System.IO;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace SAV
 {
@@ -13,15 +15,48 @@ namespace SAV
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel(); // Liga o XAML à ViewModel
+            MinhasVias = new ObservableCollection<Via>();
+
+            if (Properties.Settings.Default.TemaEscuro)
+            {
+                var conversorBrush = new BrushConverter();
+
+                Application.Current.Resources["corDestaquePrimaria"] = (Brush)conversorBrush.ConvertFrom("#1B1B1B")!;
+                Application.Current.Resources["corDestaqueSecundaria"] = (Brush)conversorBrush.ConvertFrom("#F5F5F5")!;
+                Application.Current.Resources["corDestaqueTerciaria"] = (Brush)conversorBrush.ConvertFrom("#292929")!;
+                Application.Current.Resources["corFonte"] = (Brush)conversorBrush.ConvertFrom("#F5F5F5")!;
+            }
+        }
+
+        public ObservableCollection<Via> MinhasVias { get; set; }
+
+        private Via _viaSelecionada;
+        public Via ViaSelecionada
+        {
+            get => _viaSelecionada;
+            set { _viaSelecionada = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private void AbrirConfiguracoes_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                ConfiguracoesWindow configuracoes = new ConfiguracoesWindow();
+                configuracoes.Owner = this;
+                configuracoes.ShowDialog();
+            }
         }
 
         private void Via_MouseDown(object sender, MouseButtonEventArgs e)
         {
             var polyline = sender as Polyline;
             var via = polyline.DataContext as Via;
-            var vm = this.DataContext as MainViewModel;
-            vm.ViaSelecionada = via; // Coloca a via no painel lateral
+            ViaSelecionada = via; // Coloca a via no painel lateral
         }
 
         // Mude de BotaoAbrir_Click para AbrirMapa_MouseDown
@@ -33,18 +68,22 @@ namespace SAV
 
             if (openFileDialog.ShowDialog() == true)
             {
-                // 1. Pegamos a nossa ViewModel
-                var viewModel = this.DataContext as MainViewModel;
 
                 // 2. Chamamos a função de ler o arquivo (aquela que usa o Newtonsoft.Json)
                 // Se você ainda não criou a função AbrirMapa na ViewModel, 
                 // use o código abaixo para teste rápido:
-                var pontos = new System.Windows.Media.PointCollection {
-            new Point(100, 100),
-            new Point(400, 200)
-        };
+                var pontos = new PointCollection {
+                    new Point(300, 100),
+                    new Point(400, 200),
+                    new Point(500, 100),
+                    new Point(550, 200),
+                    new Point(600, 100),
+                    new Point(720, 200),
+                    new Point(800, 100),
+                    new Point(820, 200)
+                };
 
-                viewModel.MinhasVias.Add(new Via
+                MinhasVias.Add(new Via
                 {
                     Nome = "Rua Importada",
                     Velocidade = 60,
